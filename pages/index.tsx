12591dacbde55bb30df8d7918d2c8d66a9aed564
OkadaGenya <g.okada.wilf+github@gmail.com>
2020-08-31 14:02:40 +0900

    初期描画

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Wrap = styled.p`
+  line-height: 1.7rem;
+  padding: 0 8px;
+`;
+
+export default () => (
+  <Wrap>test
+  </Wrap>
+);
